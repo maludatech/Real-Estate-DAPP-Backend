7ead87bfa678cb0fa40e2bfb13dc28a31ac4ccc8
maluda <nwitevictor7@gmail.com>
2025-03-23 14:28:01 +0100

    updated RealEstateContract

---
+++ contracts/RealEstate.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("RealEstateNFT", "REALE") {}
+
+    function mintNFT(
+        address recipient,
+        string memory tokenURI
+    ) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
