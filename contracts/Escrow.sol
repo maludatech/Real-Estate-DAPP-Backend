ee808e996f444a0bb223318bd854f96097cc28c3
maluda <nwitevictor7@gmail.com>
2025-03-25 16:31:45 +0100

    updated escrow contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,2 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+
+interface IERC721{
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id,
+    )external;
+}
+
+contract Escrow {
+    
+}
