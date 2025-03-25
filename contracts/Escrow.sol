6820d3825d913b2e452a59b4a9faf1cbcc5ee093
maluda <nwitevictor7@gmail.com>
2025-03-25 16:40:53 +0100

    updated escrow contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,14 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-interface IERC721{
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id,
-    )external;
+interface IERC721 {
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
-    
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        _seller = seller;
+        _inspector = inspector;
+        _lender = lender;
+    }
 }
